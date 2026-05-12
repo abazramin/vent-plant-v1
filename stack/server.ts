256fd13990cb087b4f040d37f45e61878260b8e9
abazramin <azr032024@gmail.com>
2026-05-12 11:24:55 +0300

    (refactor): implement sync user

--- stack/server.ts
+++ stack/server.ts
@@ -1,7 +1,31 @@
 import "server-only";
 import { StackServerApp } from "@stackframe/stack";
 import { stackClientApp } from "./client";
+import { prisma } from "@/lib/prisma";
 
 export const stackServerApp = new StackServerApp({
   inheritsFrom: stackClientApp,
 });
+
+export async function syncUser() {
+  const user = await stackServerApp.getUser();
+
+  if (!user) return null;
+
+  const dbUser = await prisma.user.upsert({
+    where: {
+      stackUserId: user.id,
+    },
+    update: {
+      email: user.primaryEmail!,
+      displayName: user.displayName,
+    },
+    create: {
+      stackUserId: user.id,
+      email: user.primaryEmail!,
+      displayName: user.displayName,
+    },
+  });
+
+  return dbUser;
+}
