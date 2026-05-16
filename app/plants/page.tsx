d60e95dd2198376cc2ad9706a9905666788c1013
abazramin <azr032024@gmail.com>
2026-05-17 01:35:03 +0300

    (refactor): immplment action plants

--- app/plants/page.tsx
+++ app/plants/page.tsx
@@ -1,3 +1,4 @@
+import { getPlants } from "@/actions/plantAction";
 import InventoryTable from "@/components/InventoryTable";
 import { stackServerApp, syncUser } from "@/stack/server";
 import { SignUp } from "@stackframe/stack";
@@ -5,13 +6,16 @@ import { SignUp } from "@stackframe/stack";
 async function page() {
   const user = await stackServerApp.getUser();
   // const app = stackServerApp.urls;
+  const plants = await getPlants();
+
+  //
   await syncUser();
   return (
     <>
       {user ? (
         <div className="mt-7 w-full mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
           <div className="lg:col-span-full">
-            <InventoryTable />
+            <InventoryTable plants={plants} />
           </div>
         </div>
       ) : (
